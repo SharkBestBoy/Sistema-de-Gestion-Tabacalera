00cf1b80e064376faabb80b9797f335b5ab92a33
dauggonzalez <dauggonzalez@uclv.cu>
2024-01-06 00:07:16 -0500

    Agregando controllers

---
+++ Backend/SGT/app/Http/Controllers/EmpleadoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empleado;
+
+class EmpleadoController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['empleados'=>Empleado::all()]);
+    }
+}
---
+++ Backend/SGT/app/Http/Controllers/VitolaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VitolaController extends Controller
+{
+    //
+}
